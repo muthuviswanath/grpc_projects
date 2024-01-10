15d2d162c3c066eff61cbfbbcd8e69b19036bcfa
muthuviswanath <muthuviswanath@outlook.com>
2024-01-10 09:38:31 +0530

    commit

--- GrpcHelloWorld/ProductGrpc/obj/Debug/net5.0/ProductGrpc.AssemblyInfo.cs
+++ GrpcHelloWorld/ProductGrpc/obj/Debug/net5.0/ProductGrpc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductGrpc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5815d728ff45be658bd213f2f653078af63cb227")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductGrpc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductGrpc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
